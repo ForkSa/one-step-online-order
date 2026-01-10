c573b21f5c60bcc50fcf3e08ed9ef498059b14e2
Shawareb <ahmedshawareb567@gmail.com>
2026-01-10 14:06:37 +0400

    fix: empty checkout

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,12 +1,30 @@
-import { ChevronRight } from "lucide-react"
+import { useAtomValue } from "jotai"
+import { ChevronRight, ShoppingCartIcon } from "lucide-react"
 
 import { useNavigate } from "react-router"
 
+import { Button } from "@/components/ui/button"
+
+import { cartSummary, storeInfoAtom } from "@/atoms"
 import CartDetails from "@/components/cart/details"
 import CheckoutForm from "@/components/forms/checkout"
 
 export default function Checkout() {
     const navigate = useNavigate()
+    const summary = useAtomValue(cartSummary)
+    const { slug } = useAtomValue(storeInfoAtom)
+
+    if (summary?.items?.length === 0) {
+        return (
+            <div className="bg-gray-50 min-h-screen px-4 flex items-center justify-center">
+                <div className="container bg-white p-6 rounded-xl flex flex-col items-center gap-y-4">
+                    <ShoppingCartIcon className="size-10 text-red-500" strokeWidth={2} />
+                    <h1 className="text-xl font-bold">لا يوجد منتجات في السلة</h1>
+                    <Button onClick={() => navigate(`/restaurant/${slug}`)}>العودة إلى المطعم</Button>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className=" relative w-full min-h-screen bg-gray-50 pb-6">
